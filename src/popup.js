6c6c3550037e2d5592aaa4f7524d5f36edeb8d54
Rztprog <45171753+rztprog@users.noreply.github.com>
2023-11-10 15:27:13 +0100

    Update to v1.1.1
    Fix(s):
    - Fixed null MeetButton
    
    Change(s):
    - Renamed "Settings" section to "Ads"
    - Updated variable case to snakeCase
    - Updated gist source code to github source code
    - Adjusted startTimer.setInterval from 300 to 200
    - Renamed updateCheckbox to updateInputElement in popup.js
    - Remove unnecessary timers
    
    New(s):
    - Add new section "Extras"
    - Add premium Outlook Logo (Top left) + Tab Title
    - Add email counter
    - Add email counter color support (rgb, rgba, hexa, color name)
    - Add descriptions when targeting properties
    - Add capability of display check all button
    - Add align title folder

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,41 @@
-function updateCheckbox(id, storageKey) {
-    const checkbox = document.getElementById(id);
-    if (checkbox) {
+const colorChecker = (ms = 100) => {
+    const findColorInput = () => {
+        const textInput = document.getElementById("numberofemailcolorInput");
+        const colorInput = document.querySelector(".numberofemailtypecolorInput");
+
+        if (textInput) {
+            textInput.addEventListener("input", function (event) {
+                chrome.storage.local.set({ numberOfEmailColor: event.target.value });
+                colorInput.style.backgroundColor = event.target.value;
+            });
+        
+            textInput.addEventListener("change", function (event) {
+                chrome.storage.local.set({ numberOfEmailColor: event.target.value });
+                colorInput.style.backgroundColor = event.target.value;
+            });
+
+            colorInput.style.backgroundColor = textInput.value;
+            clearInterval(timer);
+        }
+    }
+    const timer = setInterval(findColorInput, ms);
+}
+
+const updateInputElement = (id, storageKey) => {
+    const inputElement = document.getElementById(id);
+
+    if (inputElement) {
         chrome.storage.local.get([storageKey], function (value) {
             if (value[storageKey] !== undefined) {
-                checkbox.checked = value[storageKey];
-                checkbox.addEventListener("input", function(e) {
-                    chrome.storage.local.set({ [storageKey]: e.target.checked });
+                if (inputElement.type === 'checkbox') {
+                    inputElement.checked = value[storageKey];
+                } else {
+                    inputElement.value = value[storageKey];
+                }
+
+                inputElement.addEventListener("input", function (e) {
+                    const newValue = inputElement.type === 'checkbox' ? e.target.checked : e.target.value;
+                    chrome.storage.local.set({ [storageKey]: newValue });
                 });
             }
         });
@@ -13,11 +43,18 @@ function updateCheckbox(id, storageKey) {
 }
 
 window.onload = function() {
-    updateCheckbox("hideleftbannerInput", "hideleftbanner");
-    updateCheckbox("hidepremiumadInput", "hidepremiumad");
-    updateCheckbox("hidetopiconsInput", "hidetopicons");
+    updateInputElement("hideleftbannerInput", "hideLeftRail");
+    updateInputElement("hidepremiumadInput", "hidePremiumAd");
+    updateInputElement("hidetopiconsInput", "hideTopIcons");
+
+    colorChecker();
+    updateInputElement("addpremiumlogoInput", "premiumLogo");
+    updateInputElement("addnumberofemailInput", "addNumberOfEmail");
+    updateInputElement("numberofemailcolorInput", "numberOfEmailColor");
+    updateInputElement("numberofemailtypecolorInput", "numberOfEmailColor");
+    updateInputElement("addcheckallbuttonInput", "checkAllVisible");
+    updateInputElement("addaligntitlefolderInput", "alignTitle");
 
     let manifestData = chrome.runtime.getManifest();
-
-    document.getElementById("ext-version").textContent = "v" + manifestData.version;
+    document.querySelector(".extVersion").textContent = `v${manifestData.version}`;
 };
